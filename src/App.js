25180dde907d563bb9152edd5fd39df578b0a89b
Amiram Yassif <amiram.yassif@live.biu.ac.il>
2022-04-18 15:48:58 +0300

    updated files

--- src/App.js
+++ src/App.js
@@ -2,33 +2,14 @@ import logo from './logo.svg';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import ChatTile from './ChatTile';
+//import ChatTile from './ChatTile';
 import ChatTile2 from './ChatTile2';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button>asdasd</Button>
-        <ChatTile
-          chatName='Patrick' lastMessage='Holla Muchacho' profilePicture='patrick.jpg'
-          unreadCounter='10' timeLastMessage='19:46'></ChatTile>
-        <ChatTile chatName='Spongebob' lastMessage='Holla' profilePicture='spongebob.jpg' 
-        unreadCounter='99+' timeLastMessage='16.4.2022'></ChatTile>
         <ChatTile2></ChatTile2>
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/ChatTile2.js
+++ src/ChatTile2.js
@@ -1,23 +1,13 @@
-import { Button, ToggleButton, Flex } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import 'bootstrap'
 import './ChatTile2.css'
 
 function ChatTile2() {
     return (
-        <Button className='container' variant='light'>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">.col-md-4</div>
-                    <div class="col-md-4 ms-auto">.col-md-4 .ms-auto</div>
+        <Button className="chat-tile-container">
+                <div className='float-right'>
+                    שגשדג
                 </div>
-                <div class="row">
-                    <div class="col-md-3 ms-md-auto">.col-md-3 .ms-md-auto</div>
-                    <div class="col-md-3 ms-md-auto">.col-md-3 .ms-md-auto</div>
-                </div>
-                <div class="row">
-                    <div class="col-auto me-auto">.col-auto .me-auto</div>
-                    <div class="col-auto">.col-auto</div>
-                </div>
-            </div>
         </Button>
     );
 }
