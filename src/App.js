4e53813d8c94e5d4d8af220f04df03a2be85b779
Amiram Yassif <amiram.yassif@live.biu.ac.il>
2022-04-19 09:10:48 +0300

    import ct2->ct

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-//import ChatTile from './ChatTile';
-import ChatTile2 from './ChatTile2';
+import ChatTile from './ChatTile';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ChatTile2></ChatTile2>
+        <ChatTile
+          unreadCounter = '1'
+          profPic = 'patrick.jpg'
+          lastMessageTime = '18:09'
+          chatTitle = 'pAtRiCk'
+          lastMessage = 'JALLIFISHHHH :DDDDD'  
+        />
       </header>
     </div>
   );
