8bf19eea8dbb0286b5dd4ad92ed9b9cdcf93a79f
Amiram Yassif <amiram.yassif@live.biu.ac.il>
2022-04-24 01:02:09 +0300

    1. Managed to properly program tile list. 2. Design changes: Made tiles smaller. 3. Change in logic, avoided user struct, and used the flex JSON structure.

--- src/ChatTile.js
+++ src/ChatTile.js
@@ -15,16 +15,20 @@ import './ChatTile.css'
  */
 function ChatTile2(args) {
     return (
-        <Button className="chat-tile-container">
-            {args.unreadCounter < 1? <></>:<Badge className='notification-bubble'>{args.unreadCounter}</Badge>}
-            <img className='prof-pic' src={args.profPic}></img>
-            <div className='time-text'>
-                {args.lastMessageTime}
-            </div>
-            <div className='chat-title'>{args.chatTitle}</div>
-            <div className='last-message'>{args.lastMessage}</div>
-            
-        </Button>
+        <div>
+            <Button className="chat-tile-container">
+                {args.unreadCounter < 1 ? <></> : <Badge className='notification-bubble'>{args.unreadCounter}</Badge>}
+                <img className='prof-pic' src={args.profPic}></img>
+                <div className='time-text'>
+                    {args.lastMessageTime}
+                </div>
+                <div className='chat-title'>{args.chatTitle}</div>
+                <div className='last-message'>{args.lastMessage}</div>
+
+
+            </Button>
+            <div className='spacer' />
+        </div>
     );
 }
 
