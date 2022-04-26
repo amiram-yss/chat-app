0c72be847683d2a7d45510abc1873983cd96e534
Amiram Yassif <amiram.yassif@live.biu.ac.il>
2022-04-26 19:13:10 +0300

    some fixes + main menu starting point

---
+++ src/ChatInterface.js
@@ -0,0 +1,25 @@
+import { Row, Col } from 'react-bootstrap'
+import './ChatInterface.css'
+
+function ChatInterface() {
+    return (
+        <div className='split-screen'>
+            <div className='left'>
+                <div className='user-info'>
+
+                </div>
+                <div className='chat-list'>
+
+                </div>
+
+            </div>
+            <div className='right flex-sub-container'>
+                <div className='user-info2'/>
+                <div className='messages-container'/>
+                <div className='submit-block flex-item-bottom'/>
+            </div>
+        </div>
+    )
+}
+
+export default ChatInterface
