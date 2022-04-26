6c17cdb4ff99dc3ee13ff39550b4e809bd852eb1
amiram-yss <amiram-yss@users.noreply.github.com>
2022-04-26 20:15:52 +0300

    WROKS HERE!!!

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import { BrowserRouter, Routes, Route, Switch, Link } from 'react-router-dom';
 import RegistrationForm from './RegistrationForm';
 import {useState} from 'react';
 import Welcome from './Welcome';
+import ChatInterface from './ChatInterface'
 
 
 
@@ -21,40 +22,8 @@ import Welcome from './Welcome';
 
 function App() {
 
-  const Login = details => {
-    console.log(details);
-
-    for (let index = 0; index < users.length; index++) {
-      const element = users[index];
-      if (details.name == element.userName && details.password == element.pass) {
-        console.log("Well Done!!!");
-        setUser({
-          name: details.name
-        });
-        return;
-      }
-    }
-    console.log("NO!");
-
-  }
-
-  const [user, setUser] = useState({ name: "", nickName: "" });
-
-
   return (
-    <BrowserRouter>
-
-      <div className='App'>
-        {(user.name != "") ? (
-          <LoginForm />
-        ) : (
-          <Welcome Login={Login} />
-        )
-
-        }
-
-      </div>
-    </BrowserRouter>
+    <ChatInterface/>
   );
 }
 
