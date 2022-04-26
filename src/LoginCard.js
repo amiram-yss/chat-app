868213a0a909c24e72129c44a27b67498214dfd1
arielbarmats <ariel.barmats@gmail.com>
2022-04-26 11:21:23 +0300

    verify correct User

--- src/LoginCard.js
+++ src/LoginCard.js
@@ -1,47 +1,59 @@
 import RegistrationForm from './RegistrationForm';
 import { BrowserRouter, Routes, Route, Switch, Link } from 'react-router-dom';
 import './LoginCard.css';
+import {useState} from 'react';
 
 
-function LoginCard() {
-    //
+function LoginCard({Login}) {
+
+
+    const [details, setDetails] = useState({name:"", nickName:"", password:""});
+
+    const submitHandler = e => {
+        e.preventDefault()
+
+        Login(details);
+    }
+    
     return (
 
-        <form >
-            <div className="row">
-                <div className="col"><span className="text-danger">User Name</span></div>
-                <div className="col-9 ">
 
-                    <div className="form-outline mb-4">
-                        <input type="text" id="form3Example5" className="form-control" />
-                        <label className="form-label" htmlFor="form3Example3"></label>
+            <form onSubmit={submitHandler}>
+                <div className="row">
+                    <div className="col"><span className="text-danger">User Name</span></div>
+                    <div className="col-9 ">
+
+                        <div className="form-outline mb-4">
+                            <input type="text" id="form3Example5" className="form-control" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
+                            <label className="form-label" htmlFor="form3Example3"></label>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="row">
-                <div className="col"><span className="text-danger">Password</span></div>
-                <div className="col-9 ">
+                <div className="row">
+                    <div className="col"><span className="text-danger">Password</span></div>
+                    <div className="col-9 ">
 
-                    <div className="form-outline mb-4">
-                        <input type="password" id="form3Example2" className="form-control" />
-                        <label className="form-label" htmlFor="form3Example3"></label>
+                        <div className="form-outline mb-4">
+                            <input type="password" id="form3Example2" className="form-control" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+                            <label className="form-label" htmlFor="form3Example3"></label>
+                        </div>
                     </div>
                 </div>
-            </div>
 
 
-            <div className="row">
-                <button type="submit" className="btn btn-primary btn-block mb-4">
-                    Sign up
-                </button>
-            </div>
+                <div className="row">
+                    <button type="submit" className="btn btn-primary btn-block mb-4">
+                        Sign up
+                    </button>
+                </div>
 
-            <p >Not a member? <Link to="/Register" >Register</Link></p>
-        
+                <p >Not a member? <Link to="/Register" >Register</Link></p>
 
 
-        </form>
+
+            </form>
+
 
     )
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,61 +2,70 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './Welcome.css';
 
 import ChatTile from './ChatTile';
+import Wrong from './Wrong';
 import LoginForm from './LoginForm';
 import RegistrationForm from './RegistrationForm';
+import './server info/Users';
 
 import React from 'react';
 
 //import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Switch, Link } from 'react-router-dom';
 
-import {useState} from 'react';
+import { useState } from 'react';
 import LoginCard from './LoginCard';
 import RegisterCard from './RegisterCard';
 
+import 'react-bootstrap'
+import { Row } from 'react-bootstrap';
+import users from './server info/Users';
+
+
+function Welcome({ Login }) {
+
+
+ 
 
 
 
-function Welcome() {
     return (
-
-
 
 
         <section>
             <div className=" py-5 px-md-5 text-center welcomeText" >
-                
-                    <div className="row gx-lg-5 align-items-center">
-                        <div className="col-lg-4 col-md-4 col-sm-4 mb-5 mb-lg-0">
-                            <h1 className="my-5 display-5 fw-bold ls-tight">
-                                Welcome to the inner WhatsApp of   <br />
-                                <span className="text-warning display-3 fw-bold">---</span>
-                            </h1>
-                        
-                        </div>
-
-                        <div className="col-lg-8 col-md-8 col-sm-8 mb-5 mb-lg-3">
-                            <div className="card">
-                                <div className="card-body py-5 px-md-5">
-
-                                    <BrowserRouter>
-
-                                        <Routes>
-
-                                            <Route path="/" element={<LoginCard />}></Route>
-                                            <Route path="/register" element={<RegisterCard />}></Route>
-
-                                        </Routes>
-                                    </BrowserRouter>
 
 
-                                </div>
-                            </div>
-                        </div>
+                <div className="row gx-lg-5 align-items-center">
+                    <div className="col-lg-4 col-md-4 col-sm-4 mb-5 mb-lg-0">
+                        <h1 className="my-5 display-5 fw-bold ls-tight">
+                            Welcome to the inner WhatsApp of   <br />
+                            <span className="text-warning display-3 fw-bold">---</span>
+                        </h1>
 
                     </div>
 
-                
+                    <div className="col-lg-8 col-md-8 col-sm-8 mb-5 mb-lg-3">
+                        <div id="cardBlock" className="card ">
+                            <div className="card-body py-5 px-md-5">
+
+                                
+
+                                    <Routes>
+
+                                        <Route path="/" element={<LoginCard Login={Login} />}></Route>
+                                        <Route path="/register" element={<RegisterCard />}></Route>
+
+                                    </Routes>
+                                
+
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
 
 
             </div>
