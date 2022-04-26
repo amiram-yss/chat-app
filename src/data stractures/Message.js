204aa1dcc0c42baea80d562e934fef9a0dc5c5df
RotlevB <73072826+RotlevB@users.noreply.github.com>
2022-04-26 23:03:06 +0300

    User-Chat-Message define

--- src/data stractures/Message.js
+++ src/data stractures/Message.js
@@ -1,10 +1,10 @@
 const Type = {text, picture, record, video}
 
 class Message{
-    constructor(addresser, adressee, type, previous) {
+    constructor(addresser, adressee, type, content) {
         this.addresser = addresser
         this.adressee = adressee
         this.type = type
-        this.previous = previous
+        this.content = content
     }
 }
--- src/data stractures/User.js
+++ src/data stractures/User.js
@@ -1,3 +1,15 @@
+import users from '../server info/Users.js'
+import Chat from './Chat.js'
+
+console.log("hahaha")
+
+class UserInfo {
+    constructor(name, password) {
+        this.name = name
+        this.password = password
+    }
+}
+
 class User {
     /**
      * Single user object
@@ -5,10 +17,78 @@ class User {
      * @param {*} picture picture
      * @param {*} password password
      */
-    constructor(name, picture, password) {
+    constructor(name, picture) {
         this.name = name
         this.picture = picture
-        this.password = password
+        this.contacts = new Array()
+        this.chats = new Array()
     }
     
+    addContact(user) {
+        let isFound = false
+        for (let index = 0; index < this.contacts.length; index++) {
+            if (this.contacts[index].name === user.name && this.contacts[index].picture === user.picture) {
+                isFound = true
+                break
+            }
+        }
+        if (!isFound) {
+            this.contacts.push(user)
+            let chat = new Chat([this, user])
+            user.contacts.push(this)
+            this.chats.push(chat)
+            user.addChat(chat)
+        }        
+    }
+    createChat(users) {
+        let chat = new Chat(users)
+        for(let i = 0; i < users.length; i++){
+            users[i].addChat(chat)
+        }
+    }
+    addChat(chat) {
+        this.chats.push(chat)
+    }
 }
+
+function consoleUser(user, index) {
+    console.log("USER:")
+    console.log("===============")
+    console.log(user.name)
+    console.log("contacts:")
+    for (let i = 0; i < user.contacts.length; i++) {
+        
+        console.log(user.contacts[i].name)
+    }
+    console.log("chats:")
+    for (let i = 0; i < user.chats.length; i++) {
+        console.log("chat " + i)
+        for(let j = 0; j < user.chats[i].users.length; j++)
+        console.log(user.chats[i].users[j].name)
+    }
+}
+
+function main() {
+    var array = new Array()
+    let i
+    let j
+    for (i = 0; i < users.length; i++) {
+        array.push(new User(users[i].userName, users[i].pic))
+    }
+    for (i = 0; i < array.length; i++) {
+        for (j = 0; j < array.length; j++) {
+            if (i != j) {
+                array[i].addContact(array[j])
+            }
+        }
+    }
+    array[0].createChat(array)
+    array.forEach(consoleUser)
+}
+
+main()
+
+
+
+
+
