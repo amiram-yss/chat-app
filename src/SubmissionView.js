2d4d5e0367e20452393cd1bb7fbe42219970eb51
arielbarmats <ariel.barmats@gmail.com>
2022-04-28 13:49:33 +0300

    box of text

--- src/SubmissionView.js
+++ src/SubmissionView.js
@@ -11,34 +11,46 @@ function SubmissionView() {
 
     /**
      * <div></div>
+     * 
+     *  <AddNewContactPop/>
      *
      */
 
     return (
-        
+
         <div className="row box">
 
-            <button className="option littlrBtn " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                <i className="bi bi-paperclip"></i>
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li><a  href="#"><i className="dropdown-item bi bi-mic-fill"></i></a></li>
-                <li><a  href="#"><i className="dropdown-item bi bi-camera-reels"></i></a></li>
-                <li><a  href="#"><i className="dropdown-item bi bi-geo-alt"></i></a></li>
-                <li><a  href="#"><i className="dropdown-item bi bi-image"></i></a></li>
-
-            </ul>
+            <div className="col optionBtn">
 
 
-            <div className="col field">
-                <input type="text" className="form-control" placeholder="Type here..." />
+                <button className="optionBtn littlrBtn items" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i className="bi bi-paperclip"></i>
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a href="#"><i className="dropdown-item bi bi-mic-fill"></i></a></li>
+                    <li><a href="#"><i className="dropdown-item bi bi-camera-reels"></i></a></li>
+                    <li><a href="#"><i className="dropdown-item bi bi-geo-alt"></i></a></li>
+                    <li><a href="#"><i className="dropdown-item bi bi-image"></i></a></li>
+
+                </ul>
+
             </div>
 
-            <button className="record littlrBtn">
-                send
-            </button>
+            <div className="col field">
 
-            <AddNewContactPop/>
+
+
+
+                <input type="text" className="field items" placeholder="Type here..." />
+
+            </div>
+
+            <div className="col sendBtn">
+                <button className="littlrBtn items">
+                    <i class="bi bi-send"></i>
+                </button>
+
+            </div>
 
 
         </div>
