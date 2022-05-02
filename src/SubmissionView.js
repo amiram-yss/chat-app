d94499e8a2b638d1632c8ab32f29436cc8f9ad50
amiram-yss <amiram-yss@users.noreply.github.com>
2022-05-02 21:22:30 +0300

    Submission textbox edited.

--- src/SubmissionView.js
+++ src/SubmissionView.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './SubmissionView.css';
 import React from 'react';
 import { useState } from 'react';
-import {Button, InputGroup} from 'react-bootstrap'
+import { Button, InputGroup, Form } from 'react-bootstrap'
 import AddNewContactPop from './AddNewContactPop';
 import User from './data stractures/User';
 
@@ -12,7 +12,7 @@ import User from './data stractures/User';
 function SubmissionView(args) {
 
     const randomName = () => {
-        if(args.chat == "") {
+        if (args.chat == "") {
             console.log("here")
             alert("please select chat")
             document.getElementById("ChatList").click()
@@ -28,7 +28,7 @@ function SubmissionView(args) {
         // if(Text == ""){
         //     return
         // }
-        if(args.chat != "") {
+        if (args.chat != "") {
             let newMessage = { content: Text.text, time: new Date() }
             newMessage.addresser = args.User
             newMessage.addressee = args.chat
@@ -39,7 +39,7 @@ function SubmissionView(args) {
         else {
             alert("please select chat")
         }
-        
+
         setText({ text: "" })
     }
 
@@ -55,51 +55,61 @@ function SubmissionView(args) {
      */
 
     return (
-        
+
         <div className="row box">
             <input type="file" accept="video/*" name="" id="input_video" hidden onChange={async (e) => {
-                if(args.chat != ""){
+                if (args.chat != "") {
                     await args.chat.upload(e.target.files[0], args.User, args.chat)
                     args.REnder()
                 }
-            }}></input>
+            }} />
             <input type="file" accept="audio/*" name="" id="input_audio" hidden onChange={async (e) => {
-                if(args.chat != ""){
+                if (args.chat != "") {
                     await args.chat.upload(e.target.files[0], args.User, args.chat)
                     args.REnder()
                 }
-            }}></input>
+            }} />
             <input type="file" accept="image/*" name="" id="input_image" hidden onChange={async (e) => {
-                if(args.chat != ""){
+                if (args.chat != "") {
                     await args.chat.upload(e.target.files[0], args.User, args.chat)
                     args.REnder()
                 }
-            }}></input>
+            }} />
             <div className="col optionBtn">
                 <button className="optionBtn littlrBtn items" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" onClick={randomName}>
                     <i className="bi bi-paperclip"></i>
                 </button>
                 <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <button className="OPTbtn" onClick={()=>{
+                    <button className="OPTbtn" onClick={() => {
                         document.getElementById("input_audio").click()
                     }}>
                         <li><a href="#"><i className="dropdown-item bi bi-mic-fill" ></i></a></li>
                     </button >
-                    <button className="OPTbtn" onClick={()=>{
+                    <button className="OPTbtn" onClick={() => {
                         document.getElementById("input_video").click()
                     }}>
                         <li><a href="#"><i className="dropdown-item bi bi-camera-reels"></i></a></li>
                     </button>
-                    <button className="OPTbtn" onClick={()=>{
+                    <button className="OPTbtn" onClick={() => {
                         document.getElementById("input_image").click()
                     }}>
                         <li><a href="#"><i className="dropdown-item bi bi-image"></i></a></li>
                     </button>
                 </ul>
             </div>
-            
+
             <div className="col field">
-                <input type="text" className="field msgbox" placeholder="Type here..." onChange={e => setText({ text: e.target.value })} value={Text.text} />
+                <Form>
+                    <Form.Control
+                        className='field msgbox'
+                        type="text"
+                        placeholder="Insert message . . ."
+                        onChange={e =>
+                            setText({ text: e.target.value })
+                        }
+                        value= {Text.text}
+                    />
+                </Form>
             </div>
 
             <div className="col sendBtn">
@@ -112,7 +122,6 @@ function SubmissionView(args) {
                 }}>
                     <i className="bi bi-send"></i>
                 </button>
-
             </div>
 
 
@@ -122,6 +131,11 @@ function SubmissionView(args) {
 
 export default SubmissionView;
 
-
-
-
+/*
+<input
+                    type="input"
+                    className="field msgbox"
+                    placeholder="Type here..."
+                    onChange={e => setText({ text: e.target.value })}
+                    value={Text.text} />
+*/
