89b71b3562785c4f2b25cd1362f64dfc10b2261e
RotlevB <73072826+RotlevB@users.noreply.github.com>
2022-05-03 12:57:31 +0300

    chat upload logic

--- src/data stractures/Chat.js
+++ src/data stractures/Chat.js
@@ -11,9 +11,35 @@ class Chat {
     getLastMessage() {
         return this.messages.at(-1)
     }
+
     getAddresee(user) {
         if (user == this.users[0])
             return this.users[1]
         return this.users[0]
+
+    }
+
+    async upload(file, type, addresser, addressee) {
+        let a = await this.convertBase64(file)
+        console.log(a)
+        this.sendMessage({
+            content: "a",
+            time: "11:45",
+            addresser: addresser,
+            addressee: addressee
+        })
+    }
+
+    convertBase64(file) {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader()
+            fileReader.readAsDataURL(file)
+            fileReader.onload = () => {
+                resolve(fileReader.result)
+            }
+            fileReader.onerror = (error) => {
+                reject(error)
+            }
+        })
     }
 } export default Chat
